9849c5462f7f8aca128425bb35104c70a52fab61
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-02-25 14:26:02 +0400

    webhook test 3

--- backend/src/handlers/webhook.py
+++ backend/src/handlers/webhook.py
@@ -1,5 +1,6 @@
 import logging
 
+from context import ctx
 from fastapi import APIRouter, HTTPException, Request
 
 webhook_router = APIRouter()
@@ -16,14 +17,18 @@ async def webhook_handler(request: Request):
         modified_files = data.get("head_commit", {}).get("modified", [])
         repository_url = data.get("repository", {}).get("html_url", "")
 
-        # user = await ctx.course_repo.get_one(field="id", value=)
-        # if user is None:
-        #     raise HTTPException(status_code=404, detail="Course not found")
+        logger.info("Received git push webhook: %s", added_files)
+        logger.info("Received git push webhook: %s", removed_files)
+        logger.info("Received git push webhook: %s", modified_files)
+        logger.info("Received git push webhook: %s", repository_url)
 
-        logger.info("Received git push webhook:", added_files)
-        logger.info("Received git push webhook:", removed_files)
-        logger.info("Received git push webhook:", modified_files)
-        logger.info("Received git push webhook:", repository_url)
+        user = await ctx.user_course_repo.get_one(
+            field="url_repo", value=repository_url
+        )
+        if user is None:
+            raise HTTPException(status_code=404, detail="Course not found")
+
+        logger.info("Received git push webhook: %s", repr(user))
 
         return {"status": "Git push webhook received successfully"}
     except Exception as e:
