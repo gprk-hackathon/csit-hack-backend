a082654aae6d5486a969b2ee5ea3f6f36fb006ce
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-02-25 14:20:16 +0400

    fix allowed

--- backend/src/main.py
+++ backend/src/main.py
@@ -52,8 +52,8 @@ logger = logging.getLogger("app")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
